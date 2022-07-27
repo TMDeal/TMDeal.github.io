33c08676b0e86b1c46982fe96fd33151dc2d26b1
Trent Deal <trent.deal@gmail.com>
2022-07-27 16:15:00 -0400

    Add comments

--- data/filters/fix_images.lua
+++ data/filters/fix_images.lua
@@ -4,6 +4,10 @@ function get_webroot(meta)
     webroot = meta.webroot
 end
 
+-- When pandoc converts vimwiki links , they always end up prefixed with file:,
+-- even if local was used. This just chops of everything before /images in the
+-- src and replaces it with the webroot as defined in the files associated
+-- metadata
 function Image(el)
     local original = el.src
     local src = original:gsub('file:', "")
